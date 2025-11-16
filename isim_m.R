0f861efeaa417071e578825b1eb21b1e227606af
Sarah Bolinger <sarah.bolinger@gmail.com>
2025-11-16 08:29:57 -0600

    fix loop

--- isim_m.R
+++ isim_m.R
@@ -65,6 +65,7 @@ mets <- c("default","pmm", "rf", "cart", "caliber","passive", "stratify","cc")# 
 resp <- "HF_mis"
 col_list<- c(prVars,resp )# columns to select, as strings
 form_list <- formulas[[resp]]
+seeds <- c(613, 829, 1028, 111, 713)
 
 #########################################################################################
 
@@ -89,61 +90,67 @@ dimnames(res) <- list(sort(as.character(vars)),
                       )
 
 cat(sprintf(">>> running simulation %s times\n\n", params$nrun))
-for(run in 1:params$nrun){
-  cat(run)
-  datNA <- mkSimDat(nd = dat4sim, seeed = run, vars=vars, method = "amp", wt = TRUE, xdebug=params$xdeb, debug = params$deb, convFact = TRUE)
-  datNA <- datNA$amp
-  
-  for(x in seq_along(mets)){ # does matching by index help the trycatch statement?
-    ## *~*~*~*~*
-    # if (xdebug) cat("\n\n>>>> method:", x)
-    skiptoNext <- FALSE
+
+for (seed in seeds){
+  cat("seed=",seed)
+  for(run in 1:params$nrun){
+    cat(run)
+    datNA <- mkSimDat(nd = dat4sim, seeed = run+seed, vars=vars, method = "amp", wt = TRUE, xdebug=params$xdeb, debug = params$deb, convFact = TRUE)
+    datNA <- datNA$amp
     
-    tryCatch(
-      expr = {
-        vals <- mkImpSim(ampDat=datNA,
-                         cols=col_list,
-                         resp=resp, 
-                         form_list =form_list,
-                         vars=vars,
-                         mods=mods4sim,
-                         met=mets[x],
-                         debug = params$debug,
-                         m=params$m, 
-                         xdebug=params$xdebug,
-                         impplot=params$ipl
-                         )
-        # imp <- eval(parse(text=impCall))
-        # skiptoNext <- FALSE
-        # if(length(imp$loggedEvents > 0)) print(imp$loggedEvents)
-      },
-      error = function(e){
-        cat("\nERROR:", conditionMessage(e), "\n")
-        # next
-        # return(NULL)
-        # skiptoNext <- TRUE
-        skiptoNext <<- TRUE  # superassignment operator- not sure if necessary
-        # imp <- list(imp=NA)
-        # ret[,,y]
-        # continue()
-      }
-    )
-    if(skiptoNext) next
-    
-    vmatch <- match(rownames(vals), rownames(res)) # col 1 of vals is the row names
-    res[vmatch, mets[x], run,,]  <- vals
+    for(x in seq_along(mets)){ # does matching by index help the trycatch statement?
+      ## *~*~*~*~*
+      # if (xdebug) cat("\n\n>>>> method:", x)
+      skiptoNext <- FALSE
+      
+      tryCatch(
+        expr = {
+          vals <- mkImpSim(ampDat=datNA,
+                           cols=col_list,
+                           resp=resp, 
+                           form_list =form_list,
+                           vars=vars,
+                           mods=mods4sim,
+                           met=mets[x],
+                           debug = params$debug,
+                           m=params$m, 
+                           xdebug=params$xdebug,
+                           impplot=params$ipl
+                           )
+          # imp <- eval(parse(text=impCall))
+          # skiptoNext <- FALSE
+          # if(length(imp$loggedEvents > 0)) print(imp$loggedEvents)
+        },
+        error = function(e){
+          cat("\nERROR:", conditionMessage(e), "\n")
+          # next
+          # return(NULL)
+          # skiptoNext <- TRUE
+          skiptoNext <<- TRUE  # superassignment operator- not sure if necessary
+          # imp <- list(imp=NA)
+          # ret[,,y]
+          # continue()
+        }
+      )
+      if(skiptoNext) next
+      
+      vmatch <- match(rownames(vals), rownames(res)) # col 1 of vals is the row names
+      res[vmatch, mets[x], run,,]  <- vals
+    }
+    j <- 50
+    if(run %% j == 0){
+    # if(run %% 4 == 0){
+      begn <- run-j
+      endd <- run-0
+      nowtime <- format(Sys.time(), "%d%b%H%M")
+      fname <- paste(sprintf("out/runs%sto%s_%s.rds", begn, endd, nowtime))
+      saveRDS(res[,,begn:endd,,], fname)
+      cat(sprintf(">>>>>> saved runs %s to %s to file!", begn, endd))
+    }
+    # return(res)
   }
-  if(run %% 100 == 0){
-  # if(run %% 4 == 0){
-    begn <- run-100
-    endd <- run-0
-    nowtime <- format(Sys.time(), "%d%b%H%M")
-    fname <- paste(sprintf("out/runs%sto%s_%s.rds", begn, endd, nowtime))
-    saveRDS(res[,,begn:endd,,], fname)
-    cat(sprintf(">>>>>> saved runs %s to %s to file!", begn, endd))
-  }
-  # return(res)
 }
+
 
 # }
 # imp_sim <- runSim(fullDat=dat4sim,col_sel = col_list,mets = met_list,forms=form_list, resp = r, vars = var_list, mods = mods4sim,par=params) # don't want to set seed
